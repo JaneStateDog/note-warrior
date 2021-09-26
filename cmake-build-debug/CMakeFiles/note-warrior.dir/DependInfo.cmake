b34d09f1fcd1ee0d1da039ffa4443a87157e8ee3
jedid <jedidiahswets@gmail.com>
2021-09-25 20:15:03 -0400

    First commit. I forget to commit earlier so sorry about that. Right now it can spawn notes and block lol

---
+++ cmake-build-debug/CMakeFiles/note-warrior.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Projects/note-warrior/src/Functions.cpp" "C:/Projects/note-warrior/cmake-build-debug/CMakeFiles/note-warrior.dir/src/Functions.cpp.obj"
+  "C:/Projects/note-warrior/src/Note/Note.cpp" "C:/Projects/note-warrior/cmake-build-debug/CMakeFiles/note-warrior.dir/src/Note/Note.cpp.obj"
+  "C:/Projects/note-warrior/src/Octagon/Octagon.cpp" "C:/Projects/note-warrior/cmake-build-debug/CMakeFiles/note-warrior.dir/src/Octagon/Octagon.cpp.obj"
+  "C:/Projects/note-warrior/src/main.cpp" "C:/Projects/note-warrior/cmake-build-debug/CMakeFiles/note-warrior.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../vcpkg/installed/x64-windows/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/note-warrior.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/note-warrior.dir/src/Functions.cpp.obj"
+  "CMakeFiles/note-warrior.dir/src/Note/Note.cpp.obj"
+  "CMakeFiles/note-warrior.dir/src/Octagon/Octagon.cpp.obj"
+  "CMakeFiles/note-warrior.dir/src/main.cpp.obj"
+  "libnote-warrior.dll.a"
+  "note-warrior.exe"
+  "note-warrior.exe.manifest"
+  "note-warrior.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/note-warrior.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
